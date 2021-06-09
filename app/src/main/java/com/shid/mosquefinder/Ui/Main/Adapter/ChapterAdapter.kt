0760796cd2289f5941529ccce4527bf477fbe813
Shid Insa <theshid.insa@gmail.com>
2021-06-09 01:50:34 +0100

    adding azkars

---
+++ app/src/main/java/com/shid/mosquefinder/Ui/Main/Adapter/ChapterAdapter.kt
@@ -0,0 +1,46 @@
+package com.shid.mosquefinder.Ui.Main.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shid.mosquefinder.R
+import dev.kosrat.muslimdata.models.AzkarChapter
+import kotlinx.android.synthetic.main.item_chapter.view.*
+
+class ChapterAdapter(private val goToItemActivity : (id: Int) -> Unit) :RecyclerView.Adapter<ChapterAdapter.ChapterViewHolder>(){
+
+    var listData = ArrayList<AzkarChapter>()
+
+     fun setData(data:List<AzkarChapter>){
+        if (data == null) return
+        listData.clear()
+        listData.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChapterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_chapter,parent,false)
+        return ChapterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ChapterViewHolder, position: Int) {
+        val item = listData[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    inner class ChapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+       fun bind(item:AzkarChapter){
+          itemView.txt_chapter.text = item.chapterName
+
+           itemView.rootView.setOnClickListener(View.OnClickListener {
+               goToItemActivity(item.chapterId)
+           })
+      }
+    }
+
+}
---
+++ app/src/main/java/com/shid/mosquefinder/Ui/Main/View/ChapterActivity.kt
@@ -0,0 +1,50 @@
+package com.shid.mosquefinder.Ui.Main.View
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.shid.mosquefinder.R
+import com.shid.mosquefinder.Ui.Main.Adapter.ChapterAdapter
+import dev.kosrat.muslimdata.models.Language
+import dev.kosrat.muslimdata.repository.MuslimRepository
+import kotlinx.android.synthetic.main.activity_chapter.*
+import kotlinx.coroutines.launch
+
+class ChapterActivity : AppCompatActivity() {
+    private lateinit var adapter: ChapterAdapter
+    private var categorNum: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter)
+
+        categorNum = intent.getIntExtra("category", 1)
+        adapter = ChapterAdapter { categorNum?.let { it1 -> goToItemActivity(it1) } }
+        setUI()
+    }
+
+    private fun setUI() {
+        rv_chapter.adapter = adapter
+        categorNum?.let { getChapters(it) }
+    }
+
+    private fun goToItemActivity(chapterNum: Int) {
+        val intent = Intent(this, ItemActivity::class.java)
+        intent.putExtra("chapter", chapterNum)
+        startActivity(intent)
+    }
+
+    private fun getChapters(categoryId:Int) {
+        lifecycleScope.launch {
+            val repository = MuslimRepository(this@ChapterActivity)
+            val azkarChapters = repository.getAzkarChapters(Language.EN, categoryId)
+            if (azkarChapters != null) {
+                adapter.setData(azkarChapters)
+            }
+            Log.i("azkarChapters", "$azkarChapters")
+            Log.i("azkarChapters", "${azkarChapters?.size}")
+        }
+    }
+}
